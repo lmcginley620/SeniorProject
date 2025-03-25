83093a7debff3d01109c8745130754b0e9624c90
lmcginley620 <lm746243@sju.edu>
2025-03-25 16:22:37 -0400

    page transition animation

--- host-app/src/App.tsx
+++ host-app/src/App.tsx
@@ -1,6 +1,8 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
 import StartGamePage from './components/StartGamePage';
 import HomePage from './components/HomePage';
 import EnterTopicsPage from './components/EnterTopicsPage';
@@ -8,20 +10,31 @@ import QuestionPage from './components/QuestionPage';
 import EndGamePage from './components/EndGamePage';
 import { QuestionResults } from "./components/QuestionResults";
 import Leaderboard from "./components/leaderboard/Leaderboard";
+import PageWrapper from './components/PageWrapper';
+
+const AnimatedRoutes: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><StartGamePage /></PageWrapper>} />
+        <Route path="/lobby" element={<PageWrapper><HomePage /></PageWrapper>} />
+        <Route path="/enter-topics" element={<PageWrapper><EnterTopicsPage /></PageWrapper>} />
+        <Route path="/question" element={<PageWrapper><QuestionPage /></PageWrapper>} />
+        <Route path="/question-result" element={<PageWrapper><QuestionResults /></PageWrapper>} />
+        <Route path="/end" element={<PageWrapper><EndGamePage /></PageWrapper>} />
+        <Route path="/leaderboard" element={<PageWrapper><Leaderboard /></PageWrapper>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<StartGamePage />} />
-        <Route path="/lobby" element={<HomePage />} />
-        <Route path="/enter-topics" element={<EnterTopicsPage />} />
-        <Route path="/question" element={<QuestionPage />} />
-        <Route path="/end" element={<EndGamePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="/question-result" element={<QuestionResults />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 };
